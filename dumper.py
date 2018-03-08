e31db6a67db6e4611fdef871fcfa3351fdba8cd6
fauer4effect <kyle.fauer@gmail.com>
2018-03-07 21:20:12 -0500

    add some documentation

--- dumper.py
+++ dumper.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python3
+"""
+PYTHON_VERSION: 3.5
 
+Dumps the stack and heap from a process
+Only works on linux
+"""
 from ptrace.debugger.debugger import PtraceDebugger
 from ptrace.debugger.process import PtraceProcess
 import argparse
@@ -17,12 +22,13 @@ def dump(process, pid):
             start = int(start, 16)
             end = int(end, 16)
             dumpfile.write(read_memory(start, end, process))
-            
+
     mapfile.close()
     dumpfile.close()
 
 def start(pid):
     # need to attach to the process with ptrace before you can read it's proc/$pid/maps
+    # or be root I guess
     # https://unix.stackexchange.com/questions/6301/how-do-i-read-from-proc-pid-mem-under-linux
     # http://lkml.iu.edu/hypermail/linux/kernel/0505.0/0858.html 
     dbg = PtraceDebugger()
@@ -30,7 +36,7 @@ def start(pid):
     dump(process, pid)
     dbg.quit()
 
-    
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("pid", help="What process do you want to dump?", type=int)
